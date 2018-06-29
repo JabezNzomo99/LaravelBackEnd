920c40838df905452a4f107be9d0474f8593b0c0
JabezNzomo99 <jaybezdon@gmail.com>
2018-06-29 15:59:58 +0300

    alter_table_instructors

---
+++ database/migrations/2018_06_29_125837_alter_instructors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterInstructorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            //
+            $table->string('PhotoURL');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Instructors', function (Blueprint $table) {
+            //
+        });
+    }
+}
